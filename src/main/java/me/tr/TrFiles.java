8bb45df8d5c0ab497359c0efc7729231dfb7e810
Try___ <paolo.guccionemz@gmail.com>
2025-03-07 08:31:22 +0100

    Added some getters & removed main method.

--- src/main/java/me/tr/TrFiles.java
+++ src/main/java/me/tr/TrFiles.java
@@ -2,22 +2,27 @@ package me.tr;
 
 import me.tr.general.FileManager;
 
-import java.io.File;
-import java.io.InputStream;
-
 public class TrFiles {
+    private static TrFiles instance;
+    private FileManager fileManager;
 
-    public static void main(String[] args) {
-        FileManager fm = new FileManager();
-        String path = "C:\\Programming\\Java\\TrFiles\\src\\main\\resources";
-        //File file = fm.createFile(path + "\\config.yml");
-        //if (file == null) return;
-        InputStream is = fm.getFileInJar(path + "\\CMI-9.7.9.2.jar", "plugin.json");
-        if (is == null) return;
-        File pluginYaml = fm.saveFile(is, path + "\\plugin.json", true);
-        if (pluginYaml == null) return;
-        File pluginYaml2 = fm.saveFile(pluginYaml, path + "\\plugin2.json", true);
-        if (pluginYaml2 == null) return;
-        String fileToString = fm.saveToString(pluginYaml);
+    public FileManager getFileManager() {
+        if (fileManager == null) {
+            fileManager = new FileManager();
+        }
+        return fileManager;
     }
+
+    public static TrFiles getInstance() {
+        if (instance == null) {
+            instance = new TrFiles();
+        }
+        return instance;
+    }
+
+    public TrFiles() {
+        instance = this;
+        fileManager = new FileManager();
+    }
+
 }
