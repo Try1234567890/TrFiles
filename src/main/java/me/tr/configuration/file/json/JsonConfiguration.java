6ac131ea19b53bc13a647c9409542834e097aac2
Try___ <paolo.guccionemz@gmail.com>
2025-04-15 05:46:11 +0200

    Added Options for Json file & Comment code.

--- src/main/java/me/tr/configuration/file/json/JsonConfiguration.java
+++ src/main/java/me/tr/configuration/file/json/JsonConfiguration.java
@@ -9,14 +9,23 @@ import me.tr.general.utilities.FileUtilities;
 import java.io.File;
 import java.util.Map;
 
+/**
+ * This class represents the extension of FileConfiguration File for JSON.
+ */
 public class JsonConfiguration extends FileConfiguration {
-    Gson gson = new GsonBuilder().setPrettyPrinting().create();
+    private final Gson gson;
 
+    /**
+     * @return <b>{@code NOTHING, JSON NOT SUPPORT COMMENTS}</b>
+     */
     @Override
     public String buildHeader() {
         return "";
     }
 
+    /**
+     * @return <b>{@code NOTHING, JSON NOT SUPPORT COMMENTS}</b>
+     */
     @Override
     public String buildFooter() {
         return "";
@@ -45,6 +54,25 @@ public class JsonConfiguration extends FileConfiguration {
         }
     }
 
+    public JsonConfiguration(File file) {
+        GsonBuilder jsonBuilder = new GsonBuilder();
+        if (options().prettyPrinting()) {
+            jsonBuilder.setPrettyPrinting();
+        }
+        jsonBuilder.setDateFormat(options().dateFormat());
+        jsonBuilder.setVersion(options().version());
+        gson = jsonBuilder.create();
+        loadConfiguration(file);
+    }
+
+    protected JsonConfiguration() {
+        GsonBuilder jsonBuilder = new GsonBuilder();
+        if (options().prettyPrinting())
+            jsonBuilder.setPrettyPrinting();
+        jsonBuilder.setDateFormat(options().dateFormat());
+        jsonBuilder.setVersion(options().version());
+        gson = jsonBuilder.create();
+    }
 
     public static JsonConfiguration loadConfiguration(File file) {
         if (!FileUtilities.isJson(file)) {
@@ -60,6 +88,10 @@ public class JsonConfiguration extends FileConfiguration {
         loadConfiguration(file);
     }
 
+    @Override
+    protected void reload() {
+        reload(getFile());
+    }
 
     public static JsonConfiguration loadConfiguration(String file) {
         return loadConfiguration(new File(file));
