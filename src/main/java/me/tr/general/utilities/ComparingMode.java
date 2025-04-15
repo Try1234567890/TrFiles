411743417e3725326cc6c3991c2c5b6a0d99d874
Try___ <paolo.guccionemz@gmail.com>
2025-04-15 05:44:30 +0200

    Add Comment to all code, Added Method loadFileFromJar(...) & getFile(...), Added various ComparingMode, Improved some method.

---
+++ src/main/java/me/tr/general/utilities/ComparingMode.java
@@ -0,0 +1,12 @@
+package me.tr.general.utilities;
+
+/*
+ * This enum contains all ComparingMode that can be used to compare Strings.
+ */
+public enum ComparingMode {
+
+    EQUALS,
+    EQUALS_IGNORE_CASE,
+    CONTAINS
+
+}
