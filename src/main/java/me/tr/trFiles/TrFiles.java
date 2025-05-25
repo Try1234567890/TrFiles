fb0c4250164b5b71be46d9584e850c3eb81bea0b
Try___ <paolo.guccionemz@gmail.com>
2025-05-25 22:36:46 +0200

    Added support for Image (some comments are missing)

--- src/main/java/me/tr/trFiles/TrFiles.java
+++ src/main/java/me/tr/trFiles/TrFiles.java
@@ -1,16 +1,24 @@
 package me.tr.trFiles;
 
-import me.tr.trFiles.general.FileManager;
+import me.tr.trFiles.general.managers.FileManager;
+import me.tr.trFiles.general.managers.ImageManager;
 
 public class TrFiles {
     private static TrFiles instance;
     private FileManager fileManager;
+    private ImageManager imageManager;
 
     public FileManager getFileManager() {
         if (fileManager == null) {
             fileManager = new FileManager();
         }
         return fileManager;
+    }
+    public ImageManager getImageManager() {
+        if (imageManager == null) {
+            imageManager = new ImageManager();
+        }
+        return imageManager;
     }
 
     public static TrFiles getInstance() {
@@ -23,6 +31,7 @@ public class TrFiles {
     public TrFiles() {
         instance = this;
         fileManager = new FileManager();
+        imageManager = new ImageManager();
     }
 
 }
